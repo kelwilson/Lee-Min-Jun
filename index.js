79356e9ff2efc2fc2e273e7e9f8b09b05f6320a5
Kelwilson <kelwilson_achienu@icloud.com>
2025-01-10 17:52:19 -0800

    Added description to the images

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const closeBtn= document.querySelector('.fa-xmark')
 const imageWrapper = document.getElementById('wrapper')
 const fullImg = document.getElementById('full-img')
 const imgName = document.querySelector('.image-name')
+const describe = document.querySelector('.description')
 console.log(imageWrapper)
 
 // btn.addEventListener('click', function () {
@@ -19,99 +20,99 @@ const collaborations = [
   {
     designName: "GORGIO ARMANI",
     designImg: "./suits/suit0.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: " VALENTINO GARAVANI",
     designImg: "./suits/suit1.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "DONATELLA VERSACE",
     designImg: "./suits/suit2.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "CHRISTAIN DIOR",
     designImg: "./suits/suit3.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "LOUIS VUITTON",
     designImg: "./suits/suit4.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "COCO CHANEL",
     designImg: "./suits/suit5.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   {
     designName: "SAINT LAURENT",
     designImg: "./suits/suit6.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
 
   {
     designName: "TOM FORD",
     designImg: "./suits/suit7.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "RALPH LAUREN",
     designImg: "./suits/suit8.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   {
     designName: "BETSEY JOHNSON",
     designImg: "./suits/suit9.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "CALVIN KLEIN",
     designImg: "./suits/suit10.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "CHRISTAIN LOUBOUTIN",
     designImg: "./suits/suit11.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   {
     designName: "PIERRE CARDIN",
     designImg: "./suits/suit12.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "ALEXANDER MCQUEEN",
     designImg: "./suits/suit13.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "MUICCIA PRADA",
     designImg: "./suits/suit14.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   {
     designName: "KARL LAGERFIELD",
     designImg: "./suits/suit15.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
   
   {
     designName: "MARC JACOBS",
     designImg: "./suits/suit16.png",
-    description: "",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quidem unde in nam sequi non?",
   },
 ];
 
@@ -124,7 +125,7 @@ for (let i = 0; i < collaborations.length; i++) {
   <h1 class="card-title"> ${collaborations[i].designName}</h1>
   <div>
   <img class="design-img" src="${collaborations[i].designImg}" alt="">
-  <p class="card-content> ${collaborations[i].description}</p>
+  <p class="card-content> ${collaborations[i].description}</p> 
   </div>
   
   
@@ -165,6 +166,7 @@ function openModal(pic, pictext,description){
   imageWrapper.style.display = 'flex'
   fullImg.src = pic
   imgName.innerHTML = pictext 
+  describe.innerHTML = description
 }
 
 // close modal function
